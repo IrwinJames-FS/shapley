4357fa77a38fee3bb4f9c27a1ee92ceb264beeb3
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2024-08-22 13:14:14 -0400

    Updated the property descriptions

--- src/components/Geometric/types.ts
+++ src/components/Geometric/types.ts
@@ -7,6 +7,11 @@ export type GeometricProps = Omit<GeometryDescriptor, "aspectRatio" | "geometry"
 	 * If this is checked then I assume the viewbox is 0 0 1 1
 	 */
 	objectBounding?: boolean,
+
+	/**
+	 * By setting this to true a clip path is not used to limit the shape.
+	 */
 	noclip?: boolean,
+
 	geometry?: Points | Points[]
 }
