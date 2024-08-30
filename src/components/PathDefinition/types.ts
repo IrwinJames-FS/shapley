788ce8e3103fba405f71a902d39acffcfdd72de2
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2024-08-29 20:08:13 -0400

    Updated some of the documentation but it has become apparent that inheritence needs to be a used more.

--- src/components/PathDefinition/types.ts
+++ src/components/PathDefinition/types.ts
@@ -1,9 +1,23 @@
 import { ComponentPropsWithoutRef } from "react";
 
 export type PathDefinitionProps = ComponentPropsWithoutRef<"path"> & {
+	/**
+	 * The no clip command is a flag that can be used to prevent a path from being automatically closed with the *z* command
+	 */
 	noclip?: boolean
+	
+	/**
+	 * A path definition must be provided a unique id. This is used to reference the path.
+	 */
 	id: string
+
+	/**
+	 * The options method will be a place to tweak the rendering beyond the Points path command method. 
+	 */
 	options?: {
+		/**
+		 * Custom clip path properties can be passed to the clip path component that is used to refine the stroke.
+		 */
 		clipPath?: Omit<ComponentPropsWithoutRef<"clipPath">, "id">
 	}
 };
--- src/components/PolygonDefinition/types.ts
+++ src/components/PolygonDefinition/types.ts
@@ -1,7 +1,17 @@
 import { GeometryDefinitionProps } from "../GeometryDefinition/types";
 
 export type PolygonDefinitionProps = Omit<GeometryDefinitionProps, "geometry"> & {
+	/**
+	 * The number of sides used to generate the polygon
+	 */
 	sides?: number
+	/**
+	 * The degrees of rotation to be applied to the shape.
+	 */
 	rotation?: number
+
+	/**
+	 * The Corner Radius
+	 */
 	cornerRadius?: number | number[]
 }
