aa47f6a4d3507d183ac138ff3d744ff4a56447ab
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2024-09-11 13:27:04 -0400

    Updated packaging method because the css was getting lost in translation.

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 // rollup.config.js
-import resolve from "@rollup/plugin-node-resolve";
+import resolve, { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import terser from "@rollup/plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import sass from 'rollup-plugin-sass'
+import postcss from 'postcss';
 import packageJson from "./package.json";
+import autoprefixer from "autoprefixer";
 
 export default [
   {
@@ -29,7 +31,13 @@ export default [
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       terser(),
-	    sass()
+	    sass({
+        insert: true,
+        processor: css=>postcss([autoprefixer])
+        .process(css)
+        .then(result=>result.css)
+      }),
+      nodeResolve({extensions:['.css']})
     ],
     external: ["react", "react-dom"],
   },
