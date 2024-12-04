74e9f6c4401bb9270184d36dc471fc36096c135b
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2024-12-04 13:57:51 -0500

    Full Command path spec support

--- src/geometry/types.ts
+++
@@ -1,9 +0,0 @@
-import { Point } from "./Point";
-
-/**
- * If a number is provided the value will be used as both x and y in the operation.
- * 
- * If an array is provided it must have at least two values. 
- * Both arrays and Point instances are treated the same. the first value represents x and the second y.
- */
-export type Pointish = number | [number, number] | Point;
